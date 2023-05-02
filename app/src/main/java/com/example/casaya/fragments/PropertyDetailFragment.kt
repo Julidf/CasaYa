31d0b3e803d96f2e9b965b3f1c22ce2562891546
Nelson Manoche <nelson.manoche@gmail.com>
2023-05-01 23:23:29 -0300

    se maqueta vista de detalle de propiedad

---
+++ app/src/main/java/com/example/casaya/fragments/PropertyDetailFragment.kt
@@ -0,0 +1,32 @@
+package com.example.casaya.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.casaya.R
+
+class PropertyDetailFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PropertyDetailFragment()
+    }
+
+    private lateinit var viewModel: PropertyDetailViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_property_detail, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(PropertyDetailViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
