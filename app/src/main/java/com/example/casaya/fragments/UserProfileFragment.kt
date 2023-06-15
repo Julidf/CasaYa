897c2e6b7872273603f23a187c7557d553956780
santiagodelbueno <santiagodelbueno26@gmail.com>
2023-06-14 21:13:22 -0300

    userPorfileData

--- app/src/main/java/com/example/casaya/fragments/UserProfileFragment.kt
+++ app/src/main/java/com/example/casaya/fragments/UserProfileFragment.kt
@@ -1,48 +1,90 @@
 package com.example.casaya.fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.casaya.R
-import com.example.casaya.viewmodels.PropertiesListViewModel
+import com.example.casaya.repositories.UserRepository
 import com.example.casaya.viewmodels.UserProfileViewModel
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class UserProfileFragment : Fragment() {
 
 
+    private lateinit var userViewModel: UserProfileViewModel
+
     lateinit var v: View
-    lateinit var btnNavigate : Button
-    private val viewModelUserProfile: UserProfileViewModel by activityViewModels()
+    private lateinit var userName: TextView
+    private lateinit var userPhone: TextView
+    private lateinit var userDireccion: TextView
+    private lateinit var userMail: TextView
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    var firebaseUser = firebaseAuth.currentUser
+    val userRef = firebaseUser?.uid
+
 
     companion object {
         fun newInstance() = UserProfileFragment()
     }
-
-    private lateinit var viewModel: UserProfileViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         v = inflater.inflate(R.layout.fragment_user_profile, container, false)
-
+        initializeView(v)
+        userViewModel = ViewModelProvider(this).get(UserProfileViewModel::class.java)
         return v
     }
 
-   // override fun onStart() {
-     //   super.onStart()
-       // btnNavigate.setOnClickListener{
-         //   val action = UserProfileFragmentDirections.
-       // }
+    private fun initializeView(view: View) {
+        userName = view.findViewById(R.id.userNameTextView)
+        userPhone = view.findViewById(R.id.userPhoneTextView)
+        userDireccion = view.findViewById(R.id.userDireccionTextView)
+        userMail = view.findViewById(R.id.userEmailTextView)
 
-    //}
+    }
+
+    override  fun onStart() {
+        super.onStart()
 
 
+            if (userRef != null) {
+                loadDataIntoView(userRef)
+            }
 
 
-}
+    }
+
+
+    private  fun loadDataIntoView(userRef: String?) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            withContext(Dispatchers.Main) {
+                val usuario = userRef?.let { userViewModel.obtenerUsuarioPorId(userRef) }
+                if (usuario != null) {
+                    // Realiza las operaciones necesarias con el objeto de usuario
+                    // usuario.displayName, usuario.email, etc.
+                    userName.text = usuario.getName()
+                    userPhone.text = usuario.getPhone()
+                    userDireccion.text = usuario.getAddress()?.getStreet() + " " + usuario.getAddress()?.getHeight() ?: "Direccion no encontrada"
+                    userMail.text = usuario.getEmail()
+                } else {
+                    Log.d("UserProfileFragment", "El usuario no existe")
+                }
+            }
+        }
+        }
+
+
+    }
+
--- app/src/main/java/com/example/casaya/repositories/UserRepository.kt
+++ app/src/main/java/com/example/casaya/repositories/UserRepository.kt
@@ -22,30 +22,32 @@ class UserRepository {
      */
     suspend fun saveUser(newUser: User) {
         try {
-            auth.createUserWithEmailAndPassword(newUser.getEmail(), newUser.getPassword())
-                .addOnCompleteListener(Executors.newSingleThreadExecutor()) { task ->
-                    if (task.isSuccessful) {
-                        Log.d("SUCCESS !!!!!!!!!!!!!!!!!!", "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        val uid = user?.uid
+            newUser.getEmail()?.let {
+                auth.createUserWithEmailAndPassword(it, newUser.getPassword()!!)
+                    .addOnCompleteListener(Executors.newSingleThreadExecutor()) { task ->
+                        if (task.isSuccessful) {
+                            Log.d("SUCCESS !!!!!!!!!!!!!!!!!!", "createUserWithEmail:success")
+                            val user = auth.currentUser
+                            val uid = user?.uid
 
-                        if (uid === null) {
-                            throw NullPointerException("el uuid es nulo, no se creó correctamente el usuario o algo se rompió :S")
+                            if (uid === null) {
+                                throw NullPointerException("el uuid es nulo, no se creó correctamente el usuario o algo se rompió :S")
+                            }
+                            newUser.setId(uid)
+                            db.collection(COLLECTION)
+                                .document(uid)
+                                .set(newUser)
+
+                        } else {
+                            Log.w("FAILURE !!!!!!!!!!!!!!!!!!!", "createUserWithEmail:failure", task.exception)
+                            Toast.makeText(
+                                context,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
                         }
-                        newUser.setId(uid)
-                        db.collection(COLLECTION)
-                            .document(uid)
-                            .set(newUser)
-
-                    } else {
-                        Log.w("FAILURE !!!!!!!!!!!!!!!!!!!", "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(
-                            context,
-                            "Authentication failed.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
                     }
-                }
+            }
             Log.d("New User", "Se agrego exitosamente el documento")
         }catch (e: Exception) {
             Log.e("Error Message", "Exception thrown: ${e.message}")
